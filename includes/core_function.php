81511baf6117b5a21060af743504b4652e35a159
leah.fuentes <leah.fuentes@site.edu.au>
2018-06-23 09:50:43 +0800

    hide bootstrap and jquery min

--- includes/core_function.php
+++ includes/core_function.php
@@ -13,10 +13,10 @@ if(!defined('ABSPATH')){
 }
 add_action('init', 'register_script');
 function register_script() {
-    wp_register_style( 'am-jquery',plugin_dir_url(dirname(__FILE__) ). 'public/js/jquery-3.3.1.min.js', false, '1.0.0', 'all');
+  //  wp_register_style( 'am-jquery',plugin_dir_url(dirname(__FILE__) ). 'public/js/jquery-3.3.1.min.js', false, '1.0.0', 'all');
     wp_register_style( 'am-item_style',plugin_dir_url(dirname(__FILE__) ). 'public/css/am-item.css', false, '1.0.0', 'all');
-    wp_register_style( 'am-item_dropdownstyle',plugin_dir_url(dirname(__FILE__) ). 'public/css/bootstrap.min.css', false, '3.2.0', 'all');
-    wp_register_script( 'am-item_dropdownjsmin',plugin_dir_url(dirname(__FILE__) ). 'public/js/bootstrap.3.3.2.js', false, '3.3.2', 'all');
+  //  wp_register_style( 'am-item_dropdownstyle',plugin_dir_url(dirname(__FILE__) ). 'public/css/bootstrap.min.css', false, '3.2.0', 'all');
+   // wp_register_script( 'am-item_dropdownjsmin',plugin_dir_url(dirname(__FILE__) ). 'public/js/bootstrap.3.3.2.js', false, '3.3.2', 'all');
     wp_register_script( 'geolocate',plugin_dir_url(dirname(__FILE__) ). 'public/js/geolocate.js', false, '', 'all');
     wp_register_script( 'geolocateapi','https://maps.googleapis.com/maps/api/js?key=AIzaSyCs9eITsZNF8-gIwrNJ0JkTOqNxRdDrMgU&libraries=places&callback=initAutocomplete', false, '', 'all');
 
@@ -27,7 +27,7 @@ function amitem_script(){
     wp_enqueue_style('am-item',plugin_dir_url(dirname(__FILE__) ).
         'public/css/am-item.css');
     wp_enqueue_style('am-item_dropdownstyle',plugin_dir_url(dirname(__FILE__) ). 'public/css/bootstrap.min.css');
-    wp_enqueue_script('am-item_dropdownjsmin',plugin_dir_url(dirname(__FILE__) ). 'public/js/bootstrap.3.3.2.js' ,array(), false, true);
+ //   wp_enqueue_script('am-item_dropdownjsmin',plugin_dir_url(dirname(__FILE__) ). 'public/js/bootstrap.3.3.2.js' ,array(), false, true);
     wp_enqueue_script('geolocate',plugin_dir_url(dirname(__FILE__) ). 'public/js/geolocate.js' ,array(), false, true);
     wp_enqueue_script('geolocateapi','https://maps.googleapis.com/maps/api/js?key=AIzaSyCs9eITsZNF8-gIwrNJ0JkTOqNxRdDrMgU&libraries=places&callback=initAutocomplete' ,array(), false, true);
 
